c02c2b239ca0cb442eae638dd09fe5444ecf7fa3
Isaac <57366286+IsaacRo8erts@users.noreply.github.com>
2023-03-25 16:18:54 +0000

    pizza order created

--- Day3/conditional_statements.py
+++ Day3/conditional_statements.py
@@ -1,13 +1,5 @@
 # Conditional Statements
 
-# print("Welcome to the rollercoaster!")
-# height = int(input("What is your height in cm? "))
-
-# if height >= 120:
-#     print("You can ride the rollercoaster!")
-# else:
-#     print("Sorry, you have to grow taller before you can ride.")
-
 # One = is an assignment, two == is comparing if right value is equal to left value
 
 num = 1
@@ -18,16 +10,31 @@ print(num == 1) # prints True
 
 print("Welcome to the rollercoaster!")
 height = int(input("What is your height in cm? "))
+bill = 0
 
 if height >= 120:
     print("You can ride the rollercoaster!")
     age = int(input("What is your age? "))
     if age < 12:
-        print("Please pay $5.")
+        bill = 5
+        print("Please pay £5.")
     elif age <=18:
-        print("Please pay $7.")
+    # Only runs if first if not true
+        bill = 7
+        print("Please pay £7.")
     else:
-        print("Please pay $12.")
+    # Runs if both if and elif aren't true
+        bill = 12
+        print("Please pay £12.")
+
+    wants_photo = input("Do you want a photo taken? Y or N. ")
+    if wants_photo == "Y":
+    # Always runs no matter the age
+        bill += 3
+
+    print(f"Your final bill is £{bill}.") 
+
 else:
+# Runs if first if condition not met
     print("Sorry, you have to grow taller before you can ride.")
 
