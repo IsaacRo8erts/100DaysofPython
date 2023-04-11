6a251977a8617dc91a9ccea70237d3439ade3192
Isaac <57366286+IsaacRo8erts@users.noreply.github.com>
2023-04-11 22:32:26 +0100

    Day 8 nearly done

---
+++ Day8/more_functions.py
@@ -0,0 +1,22 @@
+# More Functions
+
+def greet():
+    print("Hi")
+    print("My name is")
+    print("Isaac")
+
+# Function that allows for input
+
+def greet_with_name(name):
+    print(f"Hello {name}")
+    
+#greet_with_name("Angela")
+
+something = 123
+# Parameter Argument
+
+def greet_with(name, location):
+    print(f"Hello {name}")
+    print(f"What is it like in {location}?")
+
+greet_with("Isaac", "Spain")
---
+++ Day8/positional_arguments.py
@@ -0,0 +1,8 @@
+# Positional Arguments, Keyword Arguments
+
+def greet_with(name, location):
+    print(f"Hello {name}")
+    print(f"What is it like in {location}?")
+
+greet_with(name="Isaac", location="Wales")
+
